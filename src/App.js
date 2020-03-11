9b58a5d7a24d79bdcfaf5d3eac27b6c4028cdf73
HoJin9622 <kiss0104040@gmail.com>
2020-03-12 02:21:00 +0900

    React router dom 설치

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function App() {
-  return <div>Main</div>;
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route exact path="/about" component={About} />
+          <Route exact path="/users" component={Users} />
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
