6ce1ffed90521667387dca099bdfb3ed34a5d6f9
HoJin9622 <kiss0104040@gmail.com>
2020-03-31 00:28:10 +0900

    Add View My Work Button

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,10 +1,36 @@
 import React from "react";
+import "./LandingPage.css";
 
 function LandingPage() {
+  const buttons = document.querySelectorAll(".viewMyWork");
+
+  buttons.forEach(btn => {
+    // 마우스가 클릭한 지점에서부터 ripples 이 발생하지 않는 문제
+    btn.addEventListener("click", function(e) {
+      let x = e.clientX - e.targetoffsetLeft;
+      let y = e.clientY - e.targetoffsetTop;
+
+      let ripples = document.createElement("span");
+      ripples.className = "ripples";
+      ripples.style.left = x + "px";
+      ripples.style.top = y + "px";
+      this.appendChild(ripples);
+
+      setTimeout(() => {
+        ripples.remove();
+      }, 1000);
+    });
+  });
+
   return (
     <div className="app" style={{ textAlign: "center" }}>
-      <span style={{ fontSize: "2rem" }}>Hello, I'm HoJin Lee.</span>
-      <span style={{ fontSize: "2rem" }}>I'm a junior web developer.</span>
+      <div style={{ fontSize: "2rem" }}>Hello, I'm HoJin Lee.</div>
+      <div style={{ fontSize: "2rem" }}>I'm a junior web developer.</div>
+      <div>
+        <a className="viewMyWork" href>
+          View my work
+        </a>
+      </div>
     </div>
   );
 }
