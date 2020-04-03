0c1d5648456ae084c41f8e8163c930763193d56d
HoJin9622 <kiss0104040@gmail.com>
2020-04-03 23:16:46 +0900

    Update Contact Page

--- client/src/components/views/Contact/Contact.js
+++ client/src/components/views/Contact/Contact.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { Input } from "antd";
+import "./Contact.css";
+
+const { TextArea } = Input;
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div className="contact__container">
+      <div style={{ textAlign: "center", fontSize: "2rem", fontWeight: "600" }}>
+        CONTACT
+      </div>
+      <hr />
+      <br />
+
+      <div style={{ textAlign: "center", fontWeight: "500" }}>
+        Have a question or want to work together?
+      </div>
+      <br />
+
+      <div>
+        <Input placeholder="Name" />
+      </div>
+      <br />
+
+      <div>
+        <Input placeholder="Enter email" />
+      </div>
+      <br />
+
+      <div>
+        <TextArea rows={4} placeholder="Your Message" />
+      </div>
+      <br />
+    </div>
+  );
 }
 
 export default Contact;
