7c2782764f050afc345fbc55526aa56d89ab18f1
HoJin9622 <kiss0104040@gmail.com>
2020-04-04 18:21:55 +0900

    Update Test Page Add Services

--- README.md
+++ README.md
@@ -10,6 +10,15 @@ https://webdir.tistory.com/486
 
 내용이 종횡비를 유지하면서 정의된 너비와 높이를 가득 채울때까지 확대된다.
 
+    background-size: cover;
+    background-position: center;
+
+https://www.codingfactory.net/10559
+
+https://www.codingfactory.net/10595
+
+cover : 배경을 사용하는 요소를 다 채울 수 있게 이미지를 확대 또는 축소합니다. 가로 세로 비율을 유지합니다.
+
     position: relative;
     position: absolute;
 
--- client/src/components/views/TestPage/TestPage.js
+++ client/src/components/views/TestPage/TestPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Banner.css";
 import "./About.css";
+import "./Services.css";
 
 function TestPage() {
   return (
@@ -26,6 +27,7 @@ function TestPage() {
           </p>
         </div>
       </div>
+
       {/* About */}
       <section className="about">
         <div className="contentBx">
@@ -49,7 +51,38 @@ function TestPage() {
         </div>
         <div className="imgBx"></div>
       </section>
+
       {/*  Services */}
+      <section className="services">
+        <h2 className="heading">Our Services</h2>
+        <p className="text">
+          At Success by Design, we understand how important it is for educators
+          to find the right tools for student success. When it comes to buying
+          materials in bulk, teachers and campus administrators feel the weight
+          of that responsibility twice over. No one wants to spend their limited
+          resources on a product their students are just going to set aside.
+          <div className="container">
+            <div className="serviceBx">
+              <div>
+                <img src="/images/icon1.png" />
+                <h2>Design</h2>
+              </div>
+            </div>
+            <div className="serviceBx">
+              <div>
+                <img src="/images/icon2.png" />
+                <h2>Development</h2>
+              </div>
+            </div>
+            <div className="serviceBx">
+              <div>
+                <img src="/images/icon3.png" />
+                <h2>Branding</h2>
+              </div>
+            </div>
+          </div>
+        </p>
+      </section>
 
       {/*   Technology */}
 
