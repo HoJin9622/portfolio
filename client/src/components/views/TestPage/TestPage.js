765e141775fdc465cac86a6756a57354b2399c00
HoJin9622 <kiss0104040@gmail.com>
2020-04-04 17:08:11 +0900

    Update Test Page About

--- client/src/components/views/TestPage/TestPage.js
+++ client/src/components/views/TestPage/TestPage.js
@@ -1,40 +1,64 @@
 import React from "react";
-import "./TestPage.css";
+import "./Banner.css";
+import "./About.css";
 
 function TestPage() {
   return (
-    /* Banner */
-    <div className="banner">
-      <header>
-        <a href="#" className="logo">
-          Brand Name
-        </a>
-        <div className="toggle"></div>
-      </header>
-      <img src="/images/bg.jpg" />
-      <div className="content">
-        <h2>Sucess by Design</h2>
-        <p>
-          At Success by Design, we understand how important it is for educators
-          to find the right tools for student success. When it comes to buying
-          materials in bulk, teachers and campus administrators feel the weight
-          of that responsibility twice over. No one wants to spend their limited
-          resources on a product their students are just going to set aside.
-        </p>
+    <div>
+      {/* Banner */}
+      <div className="banner">
+        <header>
+          <a href="#" className="logo">
+            Brand Name
+          </a>
+          <div className="toggle"></div>
+        </header>
+        <img src="/images/bg.jpg" />
+        <div className="content">
+          <h2>Sucess by Design</h2>
+          <p>
+            At Success by Design, we understand how important it is for
+            educators to find the right tools for student success. When it comes
+            to buying materials in bulk, teachers and campus administrators feel
+            the weight of that responsibility twice over. No one wants to spend
+            their limited resources on a product their students are just going
+            to set aside.
+          </p>
+        </div>
       </div>
+      {/* About */}
+      <section className="about">
+        <div className="contentBx">
+          <h2 className="heading">About Us</h2>
+          <p className="text">
+            At Success by Design, we understand how important it is for
+            educators to find the right tools for student success. When it comes
+            to buying materials in bulk, teachers and campus administrators feel
+            the weight of that responsibility twice over. No one wants to spend
+            their limited resources on a product their students are just going
+            to set aside.
+            <br />
+            <br />
+            At Success by Design, we understand how important it is for
+            educators to find the right tools for student success. When it comes
+            to buying materials in bulk, teachers and campus administrators feel
+            the weight of that responsibility twice over. No one wants to spend
+            their limited resources on a product their students are just going
+            to set aside.
+          </p>
+        </div>
+        <div className="imgBx"></div>
+      </section>
+      {/*  Services */}
+
+      {/*   Technology */}
+
+      {/* Testimonials */}
+
+      {/* Contact */}
+
+      {/*       Footer */}
     </div>
-
-    /* About */
-
-    /* Services */
-
-    /* Technology */
-
-    /* Testimonials */
-
-    /* Contact */
-
-    /* Footer */
   );
 }
 
