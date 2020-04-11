7721e08276324534dffde317d430cfc84773dce1
HoJin9622 <kiss0104040@gmail.com>
2020-04-11 21:53:20 +0900

    Delete TestPage NavBar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -13,23 +13,51 @@ import Contact from "./views/Contact/Contact";
 import TestPage2 from "./views/TestPage/TestPage2/TestPage2";
 
 function App() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <NavBar />
-      <div style={{ paddingTop: "69px", minHeight: "calc(100vh - 80px)" }}>
-        <Switch>
-          <Route exact path="/" component={Auth(LandingPage, null)} />
-          <Route exact path="/login" component={Auth(LoginPage, false)} />
-          <Route exact path="/register" component={Auth(RegisterPage, false)} />
-          <Route exact path="/about" component={Auth(AboutPage, null)} />
-          <Route exact path="/test1" component={Auth(TestPage1, null)} />
-          <Route exact path="/portfolio" component={Auth(Portfolio, null)} />
-          <Route exact path="/contact" component={Auth(Contact, null)} />
-          <Route exact path="/test2" component={Auth(TestPage2, null)} />
-        </Switch>
-      </div>
-    </Suspense>
-  );
+  const currentURL = window.location.pathname;
+  if (currentURL.includes("test")) {
+    return (
+      <Suspense fallback={<div>Loading...</div>}>
+        <div>
+          <Switch>
+            <Route exact path="/" component={Auth(LandingPage, null)} />
+            <Route exact path="/login" component={Auth(LoginPage, false)} />
+            <Route
+              exact
+              path="/register"
+              component={Auth(RegisterPage, false)}
+            />
+            <Route exact path="/about" component={Auth(AboutPage, null)} />
+            <Route exact path="/test1" component={Auth(TestPage1, null)} />
+            <Route exact path="/portfolio" component={Auth(Portfolio, null)} />
+            <Route exact path="/contact" component={Auth(Contact, null)} />
+            <Route exact path="/test2" component={Auth(TestPage2, null)} />
+          </Switch>
+        </div>
+      </Suspense>
+    );
+  } else {
+    return (
+      <Suspense fallback={<div>Loading...</div>}>
+        <NavBar />
+        <div style={{ paddingTop: "69px", minHeight: "calc(100vh - 80px)" }}>
+          <Switch>
+            <Route exact path="/" component={Auth(LandingPage, null)} />
+            <Route exact path="/login" component={Auth(LoginPage, false)} />
+            <Route
+              exact
+              path="/register"
+              component={Auth(RegisterPage, false)}
+            />
+            <Route exact path="/about" component={Auth(AboutPage, null)} />
+            <Route exact path="/test1" component={Auth(TestPage1, null)} />
+            <Route exact path="/portfolio" component={Auth(Portfolio, null)} />
+            <Route exact path="/contact" component={Auth(Contact, null)} />
+            <Route exact path="/test2" component={Auth(TestPage2, null)} />
+          </Switch>
+        </div>
+      </Suspense>
+    );
+  }
 }
 
 export default App;
