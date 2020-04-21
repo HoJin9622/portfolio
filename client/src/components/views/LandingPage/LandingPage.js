fe0ed91ffe22f0f4e80be2c82580e1cd597f95e4
HoJin9622 <kiss0104040@gmail.com>
2020-04-21 23:43:23 +0900

    Update Landing Page

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,9 +1,10 @@
 import React from "react";
-import "./LandingPage.css";
+import Banner from "./Section/Banner";
 
 function LandingPage() {
   return (
     <div className="app" style={{ textAlign: "center" }}>
+      <Banner />
       <div style={{ fontSize: "2rem" }}>Hello, I'm HoJin Lee.</div>
       <div style={{ fontSize: "2rem" }}>I'm a junior web developer.</div>
       <div>
