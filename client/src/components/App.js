55244c46dee1c4ac55d1ebd72c75f8186b51e8bd
HoJin9622 <kiss0104040@gmail.com>
2020-04-27 22:59:25 +0900

    Add TestPage3

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,10 +4,11 @@ import { createGlobalStyle } from "styled-components";
 // pages for this product
 import LandingPage from "./views/LandingPage/LandingPage.js";
 import AboutPage from "./views/AboutPage/AboutPage";
-import TestPage1 from "./views/TestPage/TestPage1/TestPage1";
 import Portfolio from "./views/Portfolio/Portfolio";
 import Contact from "./views/Contact/Contact";
+import TestPage1 from "./views/TestPage/TestPage1/TestPage1";
 import TestPage2 from "./views/TestPage/TestPage2/TestPage2";
+import TestPage3 from "./views/TestPage/TestPage3/TestPage3";
 
 const GlobalStyle = createGlobalStyle`
 @import url("https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800,900&display=swap");
@@ -28,10 +29,11 @@ function App() {
         <Switch>
           <Route exact path="/" component={LandingPage} />
           <Route exact path="/about" component={AboutPage} />
-          <Route exact path="/test1" component={TestPage1} />
           <Route exact path="/portfolio" component={Portfolio} />
           <Route exact path="/contact" component={Contact} />
+          <Route exact path="/test1" component={TestPage1} />
           <Route exact path="/test2" component={TestPage2} />
+          <Route exact path="/test3" component={TestPage3} />
         </Switch>
       </div>
     </Suspense>
---
+++ client/src/components/views/TestPage/TestPage3/TestPage3.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TestPage3() {
+  return <div>Test</div>;
+}
+
+export default TestPage3;
