2a358627dbb133864f5cdba8ee9007ccdd99d2af
HoJin9622 <kiss0104040@gmail.com>
2020-04-27 22:57:34 +0900

    Delete NavBar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,13 @@
 import React, { Suspense } from "react";
 import { Route, Switch } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
 // pages for this product
 import LandingPage from "./views/LandingPage/LandingPage.js";
-import NavBar from "./views/NavBar/NavBar";
 import AboutPage from "./views/AboutPage/AboutPage";
 import TestPage1 from "./views/TestPage/TestPage1/TestPage1";
 import Portfolio from "./views/Portfolio/Portfolio";
 import Contact from "./views/Contact/Contact";
 import TestPage2 from "./views/TestPage/TestPage2/TestPage2";
-import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 @import url("https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800,900&display=swap");
@@ -22,42 +21,21 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 function App() {
-  const currentURL = window.location.pathname;
-  if (currentURL.includes("test")) {
-    return (
-      <Suspense fallback={<div>Loading...</div>}>
-        <GlobalStyle />
-        <div>
-          <Switch>
-            <Route exact path="/" component={LandingPage} />
-            <Route exact path="/about" component={AboutPage} />
-            <Route exact path="/test1" component={TestPage1} />
-            <Route exact path="/portfolio" component={Portfolio} />
-            <Route exact path="/contact" component={Contact} />
-            <Route exact path="/test2" component={TestPage2} />
-          </Switch>
-        </div>
-      </Suspense>
-    );
-  } else {
-    return (
-      <Suspense fallback={<div>Loading...</div>}>
-        <GlobalStyle />
-        {/*         <NavBar /> */}
-        <div /* style={{ paddingTop: "69px", minHeight: "calc(100vh - 80px)" }} */
-        >
-          <Switch>
-            <Route exact path="/" component={LandingPage} />
-            <Route exact path="/about" component={AboutPage} />
-            <Route exact path="/test1" component={TestPage1} />
-            <Route exact path="/portfolio" component={Portfolio} />
-            <Route exact path="/contact" component={Contact} />
-            <Route exact path="/test2" component={TestPage2} />
-          </Switch>
-        </div>
-      </Suspense>
-    );
-  }
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <GlobalStyle />
+      <div>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/about" component={AboutPage} />
+          <Route exact path="/test1" component={TestPage1} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/test2" component={TestPage2} />
+        </Switch>
+      </div>
+    </Suspense>
+  );
 }
 
 export default App;
